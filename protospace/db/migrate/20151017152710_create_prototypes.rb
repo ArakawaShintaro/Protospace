95d53b5054c0d36259f232a90345eb4eeb2ff84f
Arakawa Shintaro <arashinbe@gmail.com>
2015-10-18 00:33:04 +0900

    prototypeのマイグレーションファイル生成

---
+++ protospace/db/migrate/20151017152710_create_prototypes.rb
@@ -0,0 +1,10 @@
+class CreatePrototypes < ActiveRecord::Migration
+  def change
+    create_table :prototypes do |t|
+      t.string :catchcopy
+      t.text :concept
+      t.integer :user_id
+      t.timestamps null: false
+    end
+  end
+end
