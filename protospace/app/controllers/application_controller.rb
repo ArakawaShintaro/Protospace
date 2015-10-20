977a4f65f6e2a2e62961e04f643e682de58199da
Arakawa Shintaro <arashinbe@gmail.com>
2015-10-20 12:27:06 +0900

    アバターカラム追加

--- protospace/app/controllers/application_controller.rb
+++ protospace/app/controllers/application_controller.rb
@@ -1,10 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  before_action :configure_permitted_parameters, if: :devise_controller?
 
+  private
   def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up).push(:name, :member, :profile, :works)
+    devise_parameter_sanitizer.for(:sign_up).push(:name, :member, :profile, :works, :avatar)
   end
 end
