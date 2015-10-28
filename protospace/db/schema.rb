5f5e36522c260f4436b8b6d733287281a53dbafe
Arakawa Shintaro <arashinbe@gmail.com>
2015-10-29 02:23:09 +0900

    make likes table

--- protospace/db/schema.rb
+++ protospace/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019032758) do
+ActiveRecord::Schema.define(version: 20151028171622) do
 
   create_table "images", force: :cascade do |t|
     t.string   "image",        limit: 255
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20151019032758) do
     t.integer  "prototype_id", limit: 4
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "prototypes", force: :cascade do |t|
