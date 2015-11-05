53c84ad53c15520eab288fbd8a237addfc85a51c
Arakawa Shintaro <arashinbe@gmail.com>
2015-11-06 01:51:53 +0900

    before_actionでprototypeのインスタンス取得

--- protospace/app/controllers/likes_controller.rb
+++ protospace/app/controllers/likes_controller.rb
@@ -1,19 +1,19 @@
 class LikesController < ApplicationController
+  before_action :set_prototype
 
   def create
-    # binding.pry
-    @prototype = Prototype.find(params[:prototype_id])
     @like = current_user.likes.create(prototype_id: params[:prototype_id])
-    # redirect_to @prototype
   end
 
   def destroy
-    # binding.pry
-    @prototype = Prototype.find(params[:prototype_id])
     @like = current_user.likes.find_by(prototype_id: @prototype.id)
     @like.destroy
-    @likes = Like.where(prototype_id: params[:prototype_id])
-    # redirect_to @prototype
+    # @likes = Like.where(prototype_id: params[:prototype_id])
+  end
+
+  private
+  def set_prototype
+    @prototype = Prototype.find(params[:prototype_id])
   end
 
 end
