e30ce58f70cae12095a25a29c262e2433714d23f
Arakawa Shintaro <arashinbe@gmail.com>
2015-11-06 16:34:15 +0900

    make comment table

--- protospace/db/schema.rb
+++ protospace/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151019032758) do
+ActiveRecord::Schema.define(version: 20151105174338) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text     "text",         limit: 65535
+    t.integer  "prototype_id", limit: 4
+    t.integer  "user_id",      limit: 4
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
 
   create_table "images", force: :cascade do |t|
     t.string   "image",        limit: 255
@@ -21,13 +29,21 @@ ActiveRecord::Schema.define(version: 20151019032758) do
     t.datetime "updated_at",               null: false
   end
 
+  create_table "likes", force: :cascade do |t|
+    t.integer  "user_id",      limit: 4
+    t.integer  "prototype_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "prototypes", force: :cascade do |t|
     t.string   "catchcopy",  limit: 255
     t.text     "concept",    limit: 65535
     t.integer  "user_id",    limit: 4
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+    t.datetime "created_at",                           null: false
+    t.datetime "updated_at",                           null: false
     t.string   "title",      limit: 255
+    t.integer  "like_count", limit: 4,     default: 0
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
