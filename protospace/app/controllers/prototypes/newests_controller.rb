d02f8944a802808a1f1d4258ce222027c6d10e83
Arakawa Shintaro <arashinbe@gmail.com>
2015-11-08 20:06:02 +0900

    インデント

--- protospace/app/controllers/prototypes/newests_controller.rb
+++ protospace/app/controllers/prototypes/newests_controller.rb
@@ -1,5 +1,7 @@
 class Prototypes::NewestsController < ApplicationController
-    def index
-    @prototypes = Prototype.order('id DESC').limit(8)
-    end
+
+def index
+  @prototypes = Prototype.order('id DESC').limit(8)
+end
+
 end
