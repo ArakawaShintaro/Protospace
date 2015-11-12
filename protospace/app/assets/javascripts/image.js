cfcaf30031621f7ee79c1024185392e62fd6b8c7
Arakawa Shintaro <arashinbe@gmail.com>
2015-11-12 22:25:20 +0900

    画像が表示されるようにプラグインを導入した

---
+++ protospace/app/assets/javascripts/image.js
@@ -0,0 +1,20 @@
+$(function() {
+
+  $('#main-image input:file').uploadThumbs({
+  });
+  $('#sub-image1 input:file').uploadThumbs({
+  });
+  $('#sub-image2 input:file').uploadThumbs({
+  });
+  $('#edit-main-image input:file').uploadThumbs({
+  });
+  $('#edit-sub-image1 input:file').uploadThumbs({
+  });
+  $('#edit-sub-image2 input:file').uploadThumbs({
+  });
+  $('#new-user-avatar input:file').uploadThumbs({
+  });
+  $('#edit-user-avatar input:file').uploadThumbs({
+  });
+
+});
